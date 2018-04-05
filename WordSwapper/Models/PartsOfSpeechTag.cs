cf13a67cf24a50ff6906c53e652b1703ec62debe
Kathryn Donegan <kdonegan@agile-ed.com>
2018-04-04 20:42:18 -0600

    Added link to web page with complete list of parts of speech tags.

--- WordSwapper/Models/PartsOfSpeechTag.cs
+++ WordSwapper/Models/PartsOfSpeechTag.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// List of tags: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+/// </summary>
 namespace WordSwapper.Models
 {
     public class PartsOfSpeechTag
     {
         public string Tag { get; set; }
         public string Description { get; set; }
-
-        //public PartsOfSpeechTag(string tag, string desc)
-        //{
-        //    Tag = tag;
-        //    Description = desc;
-        //}
     }
 }
